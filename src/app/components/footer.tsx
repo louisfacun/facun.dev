556c2fc02ccd7d68e143dedf19bb89f53f821e4c
Louis Facun <louisfacun@gmail.com>
2024-10-05 03:21:38 +0800

    Update footer.tsx

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="border-t border-gray-200 dark:border-gray-700">
-      <div className="max-w-2xl mx-auto px-4 py-2 text-center">
+      <div className="mx-auto max-w-2xl p-4 text-center">
         <p className="">© 2024 Louis Facun. All rights reserved.</p>
       </div>
     </footer>
