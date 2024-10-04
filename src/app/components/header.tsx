0e96842d64ff3ba301b46692c481ff3b94fa8bb5
Louis Facun <louisfacun@gmail.com>
2024-10-05 03:21:42 +0800

    Update header.tsx

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -4,9 +4,9 @@ import ThemeSwitcher from "./theme-switcher";
 
 const Header = () => {
   return (
-    <header className="bg-white dark:bg-gray-900 sticky top-0 border-b border-gray-200 dark:border-gray-700 overflow-x-auto whitespace-nowrap z-10">
-      <div className="max-w-2xl mx-auto px-4 py-2 flex justify-between items-center">
-        <a href="/" className="text-xl font-bold mr-8">
+    <header className="no-scrollbar sticky top-0 z-10 overflow-x-auto whitespace-nowrap border-b border-gray-200 bg-white dark:border-gray-700 dark:bg-gray-900">
+      <div className="mx-auto flex max-w-2xl items-center justify-between px-4 py-2">
+        <a href="/" className="mr-8 text-xl font-bold">
           Louis Facun
         </a>
 
